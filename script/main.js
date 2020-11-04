8398458b1e940c37ad8b31413d6dc341bdfb0b41
unknown <any.imh8@gmail.com>
2020-11-04 21:14:06 +0700

    overlay

--- script/main.js
+++ script/main.js
@@ -1,1 +1,18 @@
-alert(1);
+const openCatalogBtn = document.querySelector('.btn-burger'),
+      catalog = document.querySelector('.catalog'),
+      overlay = document.querySelector('.overlay'),
+      closeCatalogBtn = document.querySelector('.btn-close');  
+
+const openCatalog = () => {
+  catalog.classList.add('open');
+  overlay.classList.add('active');
+}
+
+const closeCatalog = ({ target }) => {
+  catalog.classList.remove('open');
+  overlay.classList.remove('active');
+}
+
+openCatalogBtn.addEventListener('click', openCatalog);
+closeCatalogBtn.addEventListener('click', closeCatalog);
+overlay.addEventListener('click', closeCatalog);
